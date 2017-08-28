840a99fefec59c7f5e5ddd7c80ec2c7e14cd3654
supreetsinghsoni <supreetsinghsoni@gmail.com>
2017-08-29 00:25:00 +0530

    first commit

---
+++ contact.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Contact Us</title>
+        
+        <link rel="stylesheet" href="newcss.css">
+        <style>
+            .heading{
+    font-weight:bold;
+    color:#2E4372;
+}
+        </style>
+       
+    </head>
+        <?php include 'header.php' ?>
+        <div class='content_customer'>
+            <h3 style="text-align:center;color:#2E4372;"><u>Contact Us</u></h3>
+            
+            <div class="contact">
+            <h3 style="color:#2E4372;"><u>Supreet Singh Soni</u></h3>
+            <p><span class="heading">Address - </span>House no 351, urban estate phase - 1, dugri , ludhiana - 141001.</p>
+            <p><span class="heading">Phone - </span>9781596370</p>
+            <p><span class="heading">Email - </span>supreetsinghsoni@onlinebank.com</p>
+            
+             <h3 style="color:#2E4372;"><u>Tamandeep Singh Khamba</u></h3>
+            <p><span class="heading">Address - </span>291/100 j block BRS Nagar,Ludhiana-141001.</p>
+            <p><span class="heading">Phone - </span>9803107878</p>
+            <p><span class="heading">Email - </span>tamandeepsinghkhamba@onlinebank.com</p>
+            
+             <h3 style="color:#2E4372;"><u>Simran Singh</u></h3>
+            <p><span class="heading">Address - </span>house no 84,street number 6,passi nagar,Pakhowal road, ludhiana-141001.</p>
+            <p><span class="heading">Phone - </span>9501064773</p>
+            <p><span class="heading">Email - </span>simransingh@onlinebank.com</p>
+            </div>
+            </div>
